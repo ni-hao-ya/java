d99e9785f07a4fc1a3eec62149f9a9dd162f9bbb
ni-hao-ya <55053146+ni-hao-ya@users.noreply.github.com>
2019-11-03 20:04:13 +0800

    Add files via upload

---
+++ Example503.java
@@ -0,0 +1,17 @@
+public class Example503 {
+	public static void main(String[] args) {
+		MyThread myThread = new MyThread();  // 创建MyThread的实例对象    
+		Thread thread=new Thread(myThread);  // 创建线程对象
+		thread.start();                          // 开启线程，执行线程中的run()方法
+		while (true) {
+			System.out.println("main()方法在运行");
+		 }
+	   }
+}
+class MyThread implements Runnable {
+	public void run() {          // 线程的代码段，当调用start()方法时，线程从此处开始执行
+		while (true) {
+			System.out.println("MyThread类的run()方法在运行");
+		}
+	}
+}
